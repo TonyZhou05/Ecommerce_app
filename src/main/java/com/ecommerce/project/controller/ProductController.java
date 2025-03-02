1df5b7b35914238dea65f5bf3c6e321fb1256cce
Tongli Zhou <tonglizhou0508@gmail.com>
2025-03-02 01:09:09 -0600

    add routers for user signin, signout and user details retrieval

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -57,7 +57,7 @@ public class ProductController {
 
     @PutMapping("/products/{productId}/image")
     public ResponseEntity<ProductDTO> updateProductImage(@PathVariable Long productId,
-                                                         @RequestParam("image")MultipartFile image) throws IOException {
+                                                         @RequestParam("image≠")MultipartFile image) throws IOException {
         ProductDTO updatedProduct = productService.updateProductImage(productId, image);
         return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
     }
--- src/main/java/com/ecommerce/project/security/request/LoginRequest.java
+++ src/main/java/com/ecommerce/project/security/request/LoginRequest.java
@@ -1,8 +1,14 @@
-package com.ecommerce.project.security.jwt;
+package com.ecommerce.project.security.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+    @NotBlank
     private String username;
 
+    @NotBlank
     private String password;
 
     public String getUsername() {
